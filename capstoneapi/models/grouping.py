022988dd50901632b3371c5290d4a3963923968f
Joshua Medlen <joshua246@gmail.com>
2021-03-13 13:50:05 -0600

    fixed grouping model. finished all fixtures and databse is created.

--- capstoneapi/models/grouping.py
+++ capstoneapi/models/grouping.py
@@ -6,5 +6,5 @@ from .student import Student
 
 class Grouping(models.Model):
 
-    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING, related_name="groupings")
     instructor = models.ForeignKey(Employee, on_delete=models.DO_NOTHING)
